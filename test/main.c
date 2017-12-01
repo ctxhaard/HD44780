89f7b9681eb653e6edd4d6814cd92dd0ccaf1845
Carlo Tomasin <c.tomasin@gmail.com>
2017-12-02 00:46:28 +0100

    added proximity in test

--- test/main.c
+++ test/main.c
@@ -4,15 +4,18 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-#define PACE_USEC (500000)
+//#define PACE_USEC (500000)
+#define OUT_LEN (5)
 
 #define xstr(a) str(a)
 #define str(a) #a
 
 static void do_main(int num)
 {
-	int fd;
+	int fd_lcd;
+	int fd_prox;
 	int pos;
+	char prox_name[11] = { 0x00 };
 
 	pos = 16 + (num * 6);
 
@@ -20,64 +23,88 @@ static void do_main(int num)
 
 	printf("task: %d opening %s\n", num, xstr(DEV));
 
-	fd = open(xstr(DEV), O_WRONLY);
-	if(fd < 0) {
-		perror("cannot open device\n");
+	fd_lcd = open(xstr(DEV), O_WRONLY);
+	if(fd_lcd < 0) {
+		perror("cannot open LCD device\n");
+		return;
+	}
+
+	sprintf(prox_name, "/dev/proximity%d", num);
+	printf("task: %d opening %s\n", num, prox_name);
+	fd_prox = open(prox_name, O_RDONLY);
+	if (fd_prox < 0) {
+		perror("cannot open proximity device\n");
 		return;
 	}
 
 	do {
-		char buffer[5];
+		char buffer[OUT_LEN];
 		int val;
 		int len;
 
-		if (lseek(fd, pos, SEEK_SET) < 0) {
+		if (lseek(fd_lcd, pos, SEEK_SET) < 0) {
 			perror("lseek error\n");
 			return;
 		}
 
-		val = ((long long)rand() * 1000 / RAND_MAX);
-		len = snprintf(buffer, 4, "%d", val);
+//		val = ((long long)rand() * 1000 / RAND_MAX);
+//		len = snprintf(buffer, 4, "%d", val);
+		len = read(fd_prox, buffer, OUT_LEN - 1);
+		if (len < 0) {
+			perror("read error\n");
+			return;
+		}
 		buffer[len] = '\0';
-		if (write(fd,buffer,len) < 0) {
+
+		val = atoi(buffer);
+		val /= 10;
+
+		sprintf(buffer, "%*d", (OUT_LEN-1), val);
+
+//		while (len < 6) {
+//			buffer[len++] = ' ';
+//		}
+		//buffer[6] = '\0';
+		if (write(fd_lcd,buffer,(OUT_LEN-1)) < 0) {
 			perror("write error\n");
 			return;
 		}
 		printf("task: %d => wrote [ %s ] on pos [ %d ]\n",num , buffer, pos);
-		usleep(PACE_USEC);
+//		usleep(PACE_USEC);
 	}
 	while(1);
-	close(fd);
+	close(fd_prox);
+	close(fd_lcd);
 }
 
 int main(int argc, const char *argv[])
 {
 	int n_childs = 0;
 	pid_t pid;
-	int fd;
+	int fd_lcd;
 	
 	printf("#################\nLCD test x86\n#################\n");
 	if (argc > 1) {
 		n_childs = atoi(argv[1]);
 	}
 
-	fd = open(xstr(DEV), O_WRONLY);
-	if(fd < 0) {
+	fd_lcd = open(xstr(DEV), O_WRONLY);
+	if(fd_lcd < 0) {
 		perror("cannot open device\n");
 		return -1;
 	}
 
-	if (lseek(fd, 2, SEEK_SET) < 0) {
+	if (lseek(fd_lcd, 2, SEEK_SET) < 0) {
 		perror("lseek error\n");
 		return -1;
 	}
 
-	if (write(fd,"Distance (cm)",13) < 0) {
+	if (write(fd_lcd,"Distance (cm)",13) < 0) {
 		perror("write error\n");
 		return -1;
 	}
 
-	close(fd);
+	close(fd_lcd);
 
 
 	while (n_childs) {
